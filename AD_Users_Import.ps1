061784c87c9f2f8d946ec3d3e82b4585c0b6f6cf
Jason Hiller <hiller7857@hotmail.com>
2020-07-27 17:19:37 -0400

    Initial Build for testing

---
+++ AD_Users_Import.ps1
@@ -0,0 +1,11 @@
+$UserCreations = import-csv 'C:\users\jhiller_a.corp\documents\users.csv'
+import-module ActiveDirectory
+foreach ($UserCreation in $UserCreations){
+    new-aduser -FirstName $UserCreation.FirstName -MiddleInitial $UserCreation.MiddleInitial -LastName $UserCreation.LastName -SAMAccountName $UserCreation.SAMAccountName -Password $UserCreation.Password -DisplayName $UserCreation.DisplayName -Title $UserCreation.Title -Department $UserCreation.Department -Manager $UserCreation.Manager -Email $UserCreation.Email 
+    $VerifyUserAdded = get-aduser $UserCreation.SAMAccountName
+    If ($False -ne $VerifyUserAdded) {
+        write-host '$($UserCreation.SAMAccountName) added successfully' -ForegroundColor Green
+    }else {
+        write-host '$($UserCreation.SAMAccountName) failed to be added' -ForegroundColor Red
+    }
+}
