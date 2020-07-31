f5613b4de8eec81600ae7b64f9f6f5fdccfd3233
unknown <jhiller_a@corp.hndc.com>
2020-07-31 20:37:38 +0000

    code changes

--- AD_Users_Import.ps1
+++ AD_Users_Import.ps1
@@ -1,11 +1,32 @@
-$UserCreations = import-csv 'C:\users\jhiller_a.corp\documents\userimport.csv'
-import-module ActiveDirectory
+#Import Module
+import-module ActiveDirectory -ErrorAction Stop
+
+#Import Data
+$UserCreations = import-csv 'C:\users\jhiller_a.corp\documents\userimport.csv' -ErrorAction Stop
 foreach ($UserCreation in $UserCreations){
-    new-aduser -FirstName $UserCreation.FirstName -MiddleInitial $UserCreation.MiddleInitial -LastName $UserCreation.LastName -SAMAccountName $UserCreation.SAMAccountName -Password $UserCreation.Password -DisplayName $UserCreation.DisplayName -Title $UserCreation.Title -Department $UserCreation.Department -Manager $UserCreation.Manager -Email $UserCreation.Email -Path $UserCreation.Path 
-    $VerifyUserAdded = get-aduser $UserCreation.SAMAccountName
-    If ($False -ne $VerifyUserAdded) {
-        write-host '$($UserCreation.SAMAccountName) added successfully' -ForegroundColor Green
-    }else {
-        write-host '$($UserCreation.SAMAccountName) failed to be added' -ForegroundColor Red
+    {new-aduser `
+        -NAME $($UserCreation.FirstName) $($UserCreation.LastName) `
+        -GivenName $UserCreation.FirstName `
+        -Initial $UserCreation.MiddleInitial `
+        -SurName $UserCreation.LastName `
+        -SAMAccountName $UserCreation.SAMAccountName `
+        -AccountPassword (ConvertTo-SecureString “$UserCreation.Password” -AsPlainText -force) `
+        -Title $UserCreation.Title `
+        -Department $UserCreation.Department `
+        -Enabled $False `
+        -ChangePasswordAtLogon $True 
+        #-Path $UserCreation.Path
     }
+    #$VerifyUserAdded = get-aduser -identity "$UserCreation.FirstName + " " + $UserCreation.LastName"
+}
+
+#Add Manager
+If ($False -ne $VerifyUserAdded) {
+    write-host '$($UserCreation.SAMAccountName) added successfully' -ForegroundColor Green
+}else {
+    write-host '$($UserCreation.SAMAccountName) failed to be added' -ForegroundColor Red
+}
+foreach ($UserCreation in $UserCreations){
+    set-aduser -identity $UserCreation.DisplayName -Manager $UserCreation.Manager 
+    $VerifyUserAdded = -get-aduser -identity $UserCreation.DisplayName
 }
