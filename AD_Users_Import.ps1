6aa56a4f0fab2d05583ce6a3224ca3188cd243cd
unknown <jhiller_a@CORP.HNDC.COM>
2020-07-31 21:26:12 +0000

    updated code to fix variables and verification

--- AD_Users_Import.ps1
+++ AD_Users_Import.ps1
@@ -1,32 +1,44 @@
-#Import Module
+#Define log Path and start capturing output
+$Log = C:\users\jhiller_a.corp\documents\UserCreation_$(get-date -f yyyy-MM-dd).txt
+Start-Transcript
+
+#Import Module need to create users
 import-module ActiveDirectory -ErrorAction Stop
 
 #Import Data
 $UserCreations = import-csv 'C:\users\jhiller_a.corp\documents\userimport.csv' -ErrorAction Stop
+
+#Create Accounts
+$UserCount = 1
 foreach ($UserCreation in $UserCreations){
+    write-host "Creating $($UserCreation.DisplayName) user in Active Directory"
+    Write-Progress -activity "Create new users" -status "$($UserCreation.Displayname) being added, $UserCount of $($UserCreations.count)" -PercentComplete (($UserCount / $UserCreations.count) * 100)
     {new-aduser `
-        -NAME $($UserCreation.FirstName) $($UserCreation.LastName) `
+        -NAME $UserCreation.DisplayName `
         -GivenName $UserCreation.FirstName `
         -Initial $UserCreation.MiddleInitial `
         -SurName $UserCreation.LastName `
         -SAMAccountName $UserCreation.SAMAccountName `
+        -UserPrincipalName $UserCreation.userprincipalname `
         -AccountPassword (ConvertTo-SecureString “$UserCreation.Password” -AsPlainText -force) `
         -Title $UserCreation.Title `
         -Department $UserCreation.Department `
         -Enabled $False `
-        -ChangePasswordAtLogon $True 
-        #-Path $UserCreation.Path
+        -DisplayName $UserCreation.DisplayName `
+        -ChangePasswordAtLogon $True `
+        -Path $UserCreation.Path `
     }
-    #$VerifyUserAdded = get-aduser -identity "$UserCreation.FirstName + " " + $UserCreation.LastName"
+    #Verify account exists and specify Manager
+    $VerifyUserAdded = get-aduser -identity $UserCreation.DisplayName
+    If ($Null -ne $VerifyUserAdded) {
+        write-host "$($UserCreation.SAMAccountName) added successfully" -ForegroundColor Green
+        get-aduser -filter {displayname -eq $UserCreation.DisplayName} | set-aduser -Manager $UserCreation.Manager 
+    }else {
+        write-host "$($UserCreation.SAMAccountName) failed to be added. check for errors in output log" -ForegroundColor Red
+    }
 }
+Write-Progress -activity "Create new users" -Completed
+Write-Host "Script Completed" -ForegroundColor Green
+Stop-Transcript
 
-#Add Manager
-If ($False -ne $VerifyUserAdded) {
-    write-host '$($UserCreation.SAMAccountName) added successfully' -ForegroundColor Green
-}else {
-    write-host '$($UserCreation.SAMAccountName) failed to be added' -ForegroundColor Red
-}
-foreach ($UserCreation in $UserCreations){
-    set-aduser -identity $UserCreation.DisplayName -Manager $UserCreation.Manager 
-    $VerifyUserAdded = -get-aduser -identity $UserCreation.DisplayName
-}
+
